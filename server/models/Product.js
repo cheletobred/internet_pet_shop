346aab21e6105a05d004f2705c0a6089af97a5fe
cheletobred <vicka.vorobjeva2017@yandex.ru>
2024-11-07 18:21:53 +0300

    createClassProduct

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,22 +1,5 @@
 import { Products as ProductMapping } from "./mapping.js"
 
-const pretty = (basket) => {
-    const data = {}
-    data.id = basket.id
-    data.products = []
-    if (basket.products) {
-        data.products = basket.products.map(item => {
-            return {
-                id: item.id,
-                name: item.name,
-                price: item.price,
-                quantity: item.basket_product.quantity
-            }
-        })
-    }
-    return data
-}
-
 class Product {
     static async getOne(article) {
             const product = await ProductMapping.findByPk(article)
