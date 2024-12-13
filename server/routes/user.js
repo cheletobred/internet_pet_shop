f8a9c06f195ec572888c8612eaeff5a8744c971b
Elena2004create <q62523474@gmail.com>
2024-12-14 02:50:51 +0300

    Update user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,9 +7,9 @@ router.post('/signup', UserController.signup)
 router.post('/login', UserController.login)
 router.get('/check', UserController.check)
 
-//router.get('/getall', UserController.getAll)
-//router.get('/getone/:idUser([0-9]+)', UserController.getOne)
-//router.post('/create', UserController.create)
+router.get('/getall', UserController.getAll)
+router.get('/getone/:idUser([0-9]+)', UserController.getOne)
+router.post('/create', UserController.create)
 router.put('/update/:idUser([0-9]+)', UserController.update)
 router.delete('/delete/:idUser([0-9]+)', UserController.delete)
 
